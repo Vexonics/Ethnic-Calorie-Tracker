4e6654612499dbd123d2a0686294e896af8a1bbb
Adom Boateng <adomboateng@Adoms-MacBook-Air-2670.local>
2026-06-28 21:09:54 -0400

    add TDEE calculator with goal weight and weekly rate adjustments

---
+++ src/tdee.py
@@ -0,0 +1,31 @@
+def calculate_tdee(weight, height, age, sex, activity_lvl, goal, goal_weight, weeks_to_goal):
+    weight = weight * 0.453592
+    goal_weight = goal_weight * 0.453592
+    height = height * 2.54
+
+    if sex == "Female":
+        BMR = (10 * weight) + (6.25 * height) - (5 * age) - 161
+    else: 
+        BMR = (10 * weight) + (6.25 * height) - (5 * age) + 5
+
+    activity_multipliers = {
+        "Sedentary" : 1.2,
+        "Lightly Active" : 1.375,
+        "Moderately Active" : 1.55,
+        "Active" : 1.725,
+        "Very Active" : 1.9
+    }
+
+    TDEE = BMR * activity_multipliers[activity_lvl]
+
+    weight_change = abs(weight - goal_weight)
+    weekly_rate = weight_change / weeks_to_goal
+    daily_deficit = weekly_rate * 500
+
+    if goal == "Cut":
+        TDEE = TDEE - daily_deficit
+    elif goal == "Bulk":
+        TDEE = TDEE + daily_deficit
+    else: 
+        TDEE = TDEE
+    return round(TDEE)
